ef44aa4abb61cbfcc9a07bf411cf9cb1ad3ebf48
Subhajit Ghosh <subhajitstudent8@gmail.com>
2023-03-20 22:06:30 +0530

    Testing both http server and websocket server and combined them

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
+import express from "express";
+const app = express();
 
 const wss = new WebSocketServer({
-    port: 8000
-})
+    port: 8000,
+});
 
-let clients = []
-let rooms = []
-wss.on('connection', (ws) => {
-    ws.clientId = Math.floor(Math.random() * 1532)
-    ws.roomname = "group"
+let clients = [];
+let rooms = [];
 
-    rooms.push({
-        ws: ws,
-        roomname: ws.roomname,
-        id: ws.clientId
-    })
-
-    rooms.map((clients) => {
-        console.log(clients.ws)
-    })
-    ws.send(JSON.stringify({
-        roomname: ws.roomname,
-        id: ws.clientId
-    }));
-
-    ws.on('message', (data) => {
-        const { sender, receiver } = JSON.parse(data.toString());
-
-
-        for (let client of rooms) {
-            if (client.id == receiver) {
-                client.ws.send(JSON.stringify({
-                    msg: `Hello! My Client  : ${sender}`
-                }));
-                break;
-            }
+function getEvent(ws, event) {
+    const clientEvent = JSON.parse(event);
+    let point = clientEvent.status;
+    console.log(clientEvent);
+    if (clientEvent.event == "SHIPPED") {
+        for (let i = point; i <= 100; i++) {
+            setTimeout(() => {
+                ws.send(i);
+            }, 2000);
         }
-    })
-})
+    } else if (clientEvent.event == "DELIVERY") {
+        let point = 500;
+        for (let i = 1; i <= point; i++) {
+            ws.send(i);
+        }
+    } else {
+        GiveMessage();
+    }
+}
+wss.on("connection", (ws, req) => {
+    ws.id = Math.floor(Math.random() * 124);
+    clients.push({
+        ws: ws,
+    });
+
+    ws.on("message", (data) => {
+        getEvent(ws, data);
+    });
+
+    ws.on("close", () => {
+        clients.pop();
+    });
+});
+
+function GiveMessage() {
+    clients[0].ws.send(200);
+}
+
+app.get("/", (req, res) => {
+    GiveMessage();
+    res.json({
+        msg: "Hello Brooooo",
+    });
+});
+
+app.listen(3000, () => console.log("Server is listening..."));
